bdd70d6efc62256d6b00830486043aed540df1b1
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-10-31 12:42:03 +0900

    상태를 객체로 관리하는 예제

--- demo03/src/components/Exam06.js
+++ demo03/src/components/Exam06.js
@@ -1,13 +1,41 @@
+import { useState } from "react";
 
 
 const Exam06 = () => {
+    //각각의 상태를 분리하여 관리할때
+    // const [name, setName] = useState("");
+    // const [gender, setGender] = useState("남자");
+
+    //상태를 하나의 객체로 관리할때
+    const [info, setInfo] = useState({
+        alias: "",
+        gender: "남자"
+    });
+
+    //function changeInfo(e){}
+    const changeInfo = e => {
+        //console.log(e.target)//이벤트 발생 태그 확인
+        console.log(e.target.name, e.target.value) //이름, 값 확인
+
+        //info에서 이벤트가 발생한 태그명에 해당하는 필드만 입력값으로 바꾸고 나머진 그대로 둬라!
+        // - ...info는 info의 나머지항목을 의미(rest 연산)
+        // - 객체의 []표시를 쓰면 필드명을 변수로 지정할 수 있다.
+        setInfo({
+            ...info, [e.target.name]: e.target.value
+        })
+        
+    };
 
     return (
         <>
             <h1>상태변수가 객체인경우</h1>
 
+            이름<input type="text" name="alias" value={info.alias} onChange={changeInfo} /><br />
+            성별<select value={info.gender} name="gender" onChange={changeInfo}>
+                <option>남자</option>
+                <option>여자</option>
+            </select>
 
-            
         </>
     );
 }
