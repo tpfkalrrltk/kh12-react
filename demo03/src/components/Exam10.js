429938af8ce94c232a728136f32463d1a70742ef
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-11-01 15:26:26 +0900

    객체 배열 상태 데이터 수정 처리

--- demo03/src/components/Exam10.js
+++ demo03/src/components/Exam10.js
@@ -14,11 +14,49 @@ const Exam10 = () => {
         { itemNo: 10, itemName: "하리보젤리", itemPrice: 5500, itemType: "식품", edit: false },
 
     ]);
+
+    //  줄을 수정상태로 변경하는 함수
+    // - 이 함수를 실행하려면 최소한 itemNo는 알아야 한다.
+    // - 함수를 호출할 때 이벤트 정보(e) 대신 아이템정보(product)을 전달하여 처리하도록 처리
+    const changeToEdit = (target) => {
+        //console.log(target)
+
+        const newProducts = products.map(product => {
+            if (product.itemNo === target.itemNo) {//target과 같은 번호의 상품 만큼은
+                return {
+                    ...product,//다른건 그대로 둬도
+                    edit: true//edit를 true로 바꿔라
+                };
+            }
+            return product;//나머진 현상유지
+        });
+
+        setProducts(newProducts);
+    };
+
+    //  줄의 데이터를 변경하는 함수
+    // - 어떤아이템인지(target)와 뭐라고 입력했는지(e)를 알아야한다.
+    const changeItem = (target, e) => {
+        const newProducts = products.map(product => {
+         
+            if (product.itemNo === target.itemNo) {//같은 번호를 발견한다면
+
+                return {
+                    ...product,//나머지 정보는 그대로 두고
+                    [e.target.name]: e.target.value //입력창의 이름에 해당하는 필드값을 입력값을 바꿔라
+                }
+            }
+
+            return product;
+        });
+        setProducts(newProducts);
+    };
+
     return (
         <div className="container" key={products.itemNo}>
             <h1 className="text-center mb-4 p-4 bg-black text-light"> 상품 목록</h1>
             <div className="row mb-3 ">
-                <div className="col-3 ">
+                <div className="col-2 offset-1 ">
                     <button className="btn btn-primary btn-lg w-100">추가</button>
                 </div>
             </div>
@@ -31,6 +69,7 @@ const Exam10 = () => {
                         <th className="col-3">상품명</th>
                         <th className="col-2">판매가</th>
                         <th className="col-2">분류</th>
+                        <th className="col-1">관리</th>
                     </div>
                 </tr>
 
@@ -40,11 +79,19 @@ const Exam10 = () => {
                             <div className="row mb-1 text-center">
                                 <td className="col-1 offset-1" > {index}</td>
                                 <td className="col-2">{product.itemNo}</td>
-                                <td className="col-3"> <input type="text" value={product.itemName} size={5} /></td>
+                                <td className="col-3"> <input type="text" name="itemName" className="form-control" value={product.itemName} size={7}
+                                    onChange={e => changeItem(product, e)} /></td>
                                 <td className="col-2">
-                                    <input type="text" value={product.itemPrice}  size={5}/>
+                                    <input type="text" className="form-control" name="itemPrice" value={product.itemPrice} size={5}
+                                        onChange={e => changeItem(product, e)} />
                                 </td>
-                                <td className="col-2"><input type="text" value={product.itemType} size={5} /></td>
+                                <td className="col-2"><input type="text" name="itemType" className="form-control" value={product.itemType} size={5}
+                                    onChange={e => changeItem(product, e)} /></td>
+                                <td className="col-1">
+                                    <button className="btn btn-sm btn-secondary">취소</button>
+                                    <button className="btn btn-sm btn-success ms-1">완료</button>
+                                </td>
+
                             </div>
                         </tr>
                     ) : (
@@ -55,6 +102,10 @@ const Exam10 = () => {
                                 <td className="col-3">{product.itemName}</td>
                                 <td className="col-2">{product.itemPrice}원</td>
                                 <td className="col-2">{product.itemType}</td>
+                                <td className="col-1">
+                                    <button className="btn btn-sm btn-warning" onClick={e => changeToEdit(product)}>수정</button>
+                                    <button className="btn btn-sm btn-danger ms-1">삭제</button>
+                                </td>
                             </div>
                         </tr >
                     )
