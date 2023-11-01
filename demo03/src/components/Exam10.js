38ae7221c658bb9945c66fe34d4b4b953d98935a
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-11-01 14:31:23 +0900

    수정 상태에 대한 처리

--- demo03/src/components/Exam10.js
+++ demo03/src/components/Exam10.js
@@ -2,20 +2,26 @@ import { useState } from "react"
 
 const Exam10 = () => {
     const [products, setProducts] = useState([
-        { itemNo: 1, itemName: "포켓몬스터빵", itemPrice: 500, itemType: "식품" },
-        { itemNo: 2, itemName: "허니버터칩", itemPrice: 1300, itemType: "식품" },
-        { itemNo: 3, itemName: "참이슬후레시", itemPrice: 2200, itemType: "주류" },
-        { itemNo: 4, itemName: "카스", itemPrice: 2500, itemType: "주류" },
-        { itemNo: 5, itemName: "테라", itemPrice: 1300, itemType: "주류" },
-        { itemNo: 6, itemName: "켈리", itemPrice: 1400, itemType: "주류" },
-        { itemNo: 7, itemName: "처음처럼", itemPrice: 2000, itemType: "주류" },
-        { itemNo: 8, itemName: "오징어땅콩", itemPrice: 3500, itemType: "식품" },
-        { itemNo: 9, itemName: "신라면", itemPrice: 1500, itemType: "식품" },
-        { itemNo: 10, itemName: "하리보젤리", itemPrice: 5500, itemType: "식품" }
+        { itemNo: 1, itemName: "포켓몬스터빵", itemPrice: 500, itemType: "식품", edit: false },
+        { itemNo: 2, itemName: "허니버터칩", itemPrice: 1300, itemType: "식품", edit: false },
+        { itemNo: 3, itemName: "참이슬후레시", itemPrice: 2200, itemType: "주류", edit: false },
+        { itemNo: 4, itemName: "카스", itemPrice: 2500, itemType: "주류", edit: false },
+        { itemNo: 5, itemName: "테라", itemPrice: 1300, itemType: "주류", edit: false },
+        { itemNo: 6, itemName: "켈리", itemPrice: 1400, itemType: "주류", edit: false },
+        { itemNo: 7, itemName: "처음처럼", itemPrice: 2000, itemType: "주류", edit: false },
+        { itemNo: 8, itemName: "오징어땅콩", itemPrice: 3500, itemType: "식품", edit: false },
+        { itemNo: 9, itemName: "신라면", itemPrice: 1500, itemType: "식품", edit: false },
+        { itemNo: 10, itemName: "하리보젤리", itemPrice: 5500, itemType: "식품", edit: false },
+
     ]);
     return (
         <div className="container" key={products.itemNo}>
             <h1 className="text-center mb-4 p-4 bg-black text-light"> 상품 목록</h1>
+            <div className="row mb-3 ">
+                <div className="col-3 ">
+                    <button className="btn btn-primary btn-lg w-100">추가</button>
+                </div>
+            </div>
 
             <table className="row table table-hover">
                 <tr className="col table-primary">
@@ -29,20 +35,34 @@ const Exam10 = () => {
                 </tr>
 
                 {products.map((product, index) => (
-                    <tr className="table-light ">
-                        <div className="row mb-1 text-center">
-                            <td className="col-1 offset-1" > {index}</td>
-                            <td className="col-2">{product.itemNo}</td>
-                            <td className="col-3">{product.itemName}</td>
-                            <td className="col-2">{product.itemPrice}원</td>
-                            <td className="col-2">{product.itemType}</td>
-                        </div>
-                    </tr>
+                    product.edit ? (
+                        <tr className="table-light ">
+                            <div className="row mb-1 text-center">
+                                <td className="col-1 offset-1" > {index}</td>
+                                <td className="col-2">{product.itemNo}</td>
+                                <td className="col-3"> <input type="text" value={product.itemName} size={5} /></td>
+                                <td className="col-2">
+                                    <input type="text" value={product.itemPrice}  size={5}/>
+                                </td>
+                                <td className="col-2"><input type="text" value={product.itemType} size={5} /></td>
+                            </div>
+                        </tr>
+                    ) : (
+                        <tr className="table-light ">
+                            <div className="row mb-1 text-center">
+                                <td className="col-1 offset-1" > {index}</td>
+                                <td className="col-2">{product.itemNo}</td>
+                                <td className="col-3">{product.itemName}</td>
+                                <td className="col-2">{product.itemPrice}원</td>
+                                <td className="col-2">{product.itemType}</td>
+                            </div>
+                        </tr >
+                    )
 
                 ))}
 
             </table >
-        </div>
+        </div >
     )
 }
 export default Exam10
