989e26106a9b7513795b95559e0b8c467b3d6cef
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-11-01 14:17:00 +0900

    객체 배열 상태변수 문제(아이템)

--- demo03/src/App.js
+++ demo03/src/App.js
@@ -9,6 +9,7 @@ import Exam06 from './components/Exam06';
 import Exam07 from './components/Exam07';
 import Exam08 from './components/Exam08';
 import Exam09 from './components/Exam09';
+import Exam10 from './components/Exam10';
 
 
 function App() {
@@ -33,6 +34,8 @@ function App() {
       <Exam08 />
       <hr />
       <Exam09 />
+      <hr/>
+      <Exam10/>
 
       <div className='my-5 py-5'></div>
 
