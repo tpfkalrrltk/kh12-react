96d94c97f4349b21e4bcd376d68ddb4fb726f15b
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-11-06 13:34:16 +0900

    React Router 기본 구현

--- demo05/src/App.js
+++ demo05/src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Pocketmon from './components/Pocketmon';
+import Book from './components/Book';
+import Home from './components/Home';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid'>
+
+      {/* 상단 메뉴영역 */}
+
+      <div>
+        <NavLink to={"/home"}>홈</NavLink>
+        <NavLink to={"/pocketmon"}>포켓몬스터</NavLink>
+        <NavLink to={"/book"}>도서</NavLink>
+      </div>
+
+
+      {/* 본문영역 */}
+      <div>
+        <Routes>
+          <Route exact path='/' element={<Home/>}>홈페이지</Route>
+          <Route path='/pocketmon' element={<Pocketmon/>}>포켓</Route>
+          <Route path='/book' element={<Book></Book>}>책</Route>
+        </Routes>
+      </div>
+
+
     </div>
   );
 }
--- demo05/src/index.js
+++ demo05/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "../node_modules/bootswatch/dist/yeti/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+//Router는 React앱을 여러 페이지로 분할하여 사용하도록 만드는 기술
+// -HashRouter는 주소에 해시(#)가 포함된다.
+// -BrowserRouter는 주소에 해시(#)가 포함되지 않는다.
+import { HashRouter, BrowserRouter } from 'react-router-dom'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <HashRouter>
+        <App />
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
