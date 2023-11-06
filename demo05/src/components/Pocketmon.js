444c64a32112bc029ba2ad4f44a148694d379894
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-11-06 18:23:51 +0900

    몬스터 등록 구현 중

--- demo05/src/components/Pocketmon.js
+++ demo05/src/components/Pocketmon.js
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { LiaEdit } from "react-icons/lia"
-import { AiFillDelete } from "react-icons/ai"
+import { AiFillDelete, AiOutlinePlus } from "react-icons/ai"
+import { Modal } from "bootstrap";
 
 const Pocketmon = (props) => {
     const [pocketmonList, setpocketmonList] = useState([]);
@@ -44,6 +45,29 @@ const Pocketmon = (props) => {
 
     };
 
+    //modal 관련된 처리
+    const bsModal = useRef();
+    const openModal = () => {
+        const modal = new Modal(bsModal.current);
+        modal.show();
+    };
+    const closeModal = () => {
+        const modal = Modal.getInstance(bsModal.current);
+        modal.hide();
+    };
+
+    //등록과 관련된 state
+    const [pocketmon, setPocketmon] = useState({
+        name: "",
+        type: "",
+    });
+    const changePoketmon = (e) => {
+        setPocketmon({
+            ...pocketmon,
+            [e.target.name]: e.target.value
+        });
+    };
+
     return (
         <div className="container">
 
@@ -53,8 +77,15 @@ const Pocketmon = (props) => {
                     <p>React CRUD 연습 예제</p>
                 </div>
             </div>
+            {/* 추가버튼 */}
+            <div className="row mt-4">
+                <div className="col  text-end">
+                    <button className="btn btn-success" onClick={openModal}><AiOutlinePlus />추가</button>
+                </div>
+            </div>
 
 
+            {/* 출력 위치 */}
             <div className="row mt-4">
                 <div className="col">
                     <table className="table">
@@ -101,6 +132,46 @@ const Pocketmon = (props) => {
                 </tr>
 
             })}
+
+
+
+            {/* Modal */}
+            <div className="modal fade" ref={bsModal} id="exampleModal" tabIndex="-1"
+                data-bs-backdrop="static"
+                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">포켓몬 변경</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body container">
+                            {/* 수정화면 */}
+                            <div className="row">
+                                <div className="col">
+                                    <label className="form-label">이름</label>
+                                    <input type="text" name="name" className="form-control" value={pocketmon.name} onChange={changePoketmon} />
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col">
+                                    <label className="form-label">속성</label>
+                                    <input type="text" name="type" className="form-control" value={pocketmon.type} onChange={changePoketmon} />  </div>
+                            </div>
+
+                        </div>
+                        <div className="modal-footer">
+                            {/*  자동으로 닫히는 버튼 */}
+                            {/* <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">취소</button> */}
+                            {/*  수동으로 닫히는 버튼 */}
+                            <button className="btn btn-primary" onClick={closeModal}>저장</button>
+                            <button className="btn btn-secondary" onClick={closeModal}>닫기</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
 
 
 
