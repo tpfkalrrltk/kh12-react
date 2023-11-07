9d4cd77347788ca3d9ab2daf242c3aa62ee62dab
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-11-07 13:41:26 +0900

    도서 등록 및 수정 구현

--- demo05/src/components/Book.js
+++ demo05/src/components/Book.js
@@ -1,22 +1,31 @@
 import axios from "axios"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { LiaEdit } from "react-icons/lia"
 import { AiFillDelete } from "react-icons/ai"
 
 import "./Book.css"
+import { Modal } from "bootstrap"
 
 const Book = (props) => {
     const [BookList, setBookList] = useState([]);
 
-    const reloadBook = () => {
-        axios({
+    // const reloadBook = () => {
+    //     axios({
+    //         url: "http://localhost:8080/book/",
+    //         method: "get"
+    //     }).then(response => {
+    //         //  window.alert(" 목록 불러오기가 성공했습니다.")
+    //         setBookList(response.data);
+    //     }).catch(error => { window.alert("통신 오류 발생") });
+    // }
+    const reloadBook = async () => {
+        const response = await axios({
             url: "http://localhost:8080/book/",
             method: "get"
-        }).then(response => {
-          //  window.alert(" 목록 불러오기가 성공했습니다.")
-            setBookList(response.data);
 
-        }).catch(error => { window.alert("통신 오류 발생") });
+        })
+        //window.alert(" 목록 불러오기가 성공했습니다.")
+        setBookList(response.data);
     }
 
 
@@ -24,7 +33,6 @@ const Book = (props) => {
     useEffect(() => {
         //서버에 있는 도서 정보를 불러와서 state에 반영하는 코드
         reloadBook();
-
     }, []);
 
     const deleteBook = (Book) => {
@@ -33,7 +41,7 @@ const Book = (props) => {
 
         axios({
             url: `http://localhost:8080/book/${Book.bookId}`,
-           // url: "http://localhost:8080/book/"+Book.bookId,
+            // url: "http://localhost:8080/book/"+Book.bookId,
             method: "delete"
         }).then(response => {
             reloadBook();
@@ -41,7 +49,97 @@ const Book = (props) => {
 
         })
     }
+    // 모달
+    const bsModal = useRef();
+    const openModal = () => {
+        const modal = new Modal(bsModal.current);
+        modal.show();
+    };
+    const closeModal = () => {
+        const modal = Modal.getInstance(bsModal.current);
+        modal.hide();
+        clearBook();
+    };
+    //등록 
+    const [book, setBook] = useState({
+        bookTitle: "",
+        bookPublisher: "",
+        bookPublicationDate: "",
+        bookPrice: 0,
+        bookPageCount: 0,
+        bookGenre: "",
+        bookAuthor: ""
+    })
+    const changeBook = (e) => {
+        setBook({
+            ...book,
+            [e.target.name]: e.target.value
+        })
+    };
+    const clearBook = () => {
+        setBook({
+            bookTitle: "",
+            bookPublisher: "",
+            bookPublicationDate: "",
+            bookPrice: 0,
+            bookPageCount: 0,
+            bookGenre: "",
+            bookAuthor: ""
+        })
+    }
+    // const addBook = () => {
+    //     axios({
+    //         url: "http://localhost:8080/book/",
+    //         method: "post",
+    //         data:book
+    //         //data:{...book} 이것도 가능
 
+    //     }).then(response => {
+    //         reloadBook();
+    //         closeModal();
+
+    //     }).catch(error => {
+
+    //     });
+    // }
+
+    //async함수와 await 키워드를 사용한 간소화 작업이 가능
+    // - 비동기 작업을 동기화된 코드로 작성할 수 있다.
+    const addBook = async () => {
+        const response = await axios({
+            url: "http://localhost:8080/book/",
+            method: "post",
+            data: book
+        });
+        reloadBook();
+        closeModal();
+
+    };
+
+    //수정 정보넣기
+    const editBook = (target) => {
+        setBook({ ...target })
+        openModal();
+    }
+    //수정
+    const updateBook = () => {
+     
+        const copyBook={...book};
+        delete copyBook.bookId;
+        axios({
+
+            url:`http://localhost:8080/book/${book.bookId}`,
+            method: "put",
+            data: {...book}
+        })
+            .then(response => {
+                reloadBook();
+                closeModal();
+            })
+            .catch(error => {
+
+            });
+    }
 
     return (
         <div className="container">
@@ -50,6 +148,11 @@ const Book = (props) => {
                 <div className="col offset-1">
                     <h1>도서 관리화면</h1>
                     <p>리액트 예제</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col offset-1">
+                    <button className="btn btn-success" onClick={openModal} >도서 추가</button>
                 </div>
             </div>
 
@@ -81,7 +184,7 @@ const Book = (props) => {
                                     <td className="pc-only">{Book.bookPageCount}</td>
                                     <td className="pc-only">{Book.bookGenre}</td>
                                     {/* 아이콘자리 */}
-                                    <td> <LiaEdit className="text-warning" /></td>
+                                    <td> <LiaEdit className="text-warning" onClick={e => editBook(Book)} /></td>
                                     <td><AiFillDelete className="text-danger" onClick={e => deleteBook(Book)} /></td>
                                 </tr>
                             ))}
@@ -91,6 +194,88 @@ const Book = (props) => {
                 </div>
 
             </div>
+
+            {/* Modal */}
+            <div className="modal fade" ref={bsModal} id="exampleModal" tabIndex="-1"
+                data-bs-backdrop="static"
+                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">
+                                {book.bookId === undefined ? "책 등록" : `${book.bookId}번 책 수정`}
+                            </h5>
+                            <button type="button" className="btn-close border-0 bg-transparent" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body container">
+                            {/* 수정화면 */}
+                            <div className="row">
+                                <div className="col">
+                                    <label className="form-label">책 제목</label>
+                                    <input type="text" name="bookTitle" className="form-control" value={book.bookTitle} onChange={changeBook} />
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col">
+                                    <label className="form-label">글쓴이</label>
+                                    <input type="text" name="bookAuthor" className="form-control" value={book.bookAuthor} onChange={changeBook} />  </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col">
+                                    <label className="form-label">출판사</label>
+                                    <input type="text" name="bookPublisher" className="form-control" value={book.bookPublisher} onChange={changeBook} />  </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col">
+                                    <label className="form-label">가격</label>
+                                    <input type="number" name="bookPrice" className="form-control" value={book.bookPrice} onChange={changeBook} />  </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col">
+                                    <label className="form-label">출판일</label>
+                                    <input type="date" name="bookPublicationDate" className="form-control" value={book.bookPublicationDate} onChange={changeBook} />  </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col">
+                                    <label className="form-label">페이지수</label>
+                                    <input type="number" name="bookPageCount" className="form-control" value={book.bookPageCount} onChange={changeBook} />  </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col">
+                                    <label className="form-label">장르</label>
+                                    <select type="text" name="bookGenre" className="form-control" value={book.bookGenre} onChange={changeBook} >
+                                        <option value="">==선택==</option>
+                                        <option value="판타지">판타지</option>
+                                        <option value="수기">수기</option>
+                                        <option value="소설">소설</option>
+                                        <option value="자기개발">자기개발</option>
+                                    </select></div>
+                            </div>
+
+                        </div>
+                        <div className="modal-footer">
+                            {/*  자동으로 닫히는 버튼 */}
+                            {/* <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">취소</button> */}
+                            {/*  수동으로 닫히는 버튼 */}
+
+                            {book.bookId === undefined ?
+                                <button className="btn btn-primary" onClick={addBook}>추가 </button>
+                                :
+                                <button className="btn btn-success" onClick={updateBook} >수정</button>
+                            }
+                            <button className="btn btn-secondary" onClick={closeModal}>닫기</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
 
 
         </div>
