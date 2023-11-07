de01ab3c104f1a2b0672603e16ef251fb72f4796
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-11-07 17:28:43 +0900

    무한 스크롤 구현

--- demo05/src/components/BookInfinite.js
+++ demo05/src/components/BookInfinite.js
@@ -1,12 +1,20 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import throttle from "lodash/throttle"//특정함수의 발생 주기를 설정(강제 성능 하향)
+//사용법 - throttle(원래쓰려고 했던 함수, 주기)
+import debounce from "lodash/debounce"// 특정 이벤트의 마지막만 실행되도록 설정
+//사용법 - debounce(원래쓰려고 했던 함수, 주기)
 
 const BookInfinite = () => {
     const [page, setPage] = useState(1);
     const [size, setSize] = useState(30);
     const [bookList, setBookList] = useState([]);
 
+
+    //ref를 이용해서 flag변수를 생성(화면과는 무관)
+    const loading = useRef(false);
     const loadBook = () => {
+        loading.current = true;//로딩중으로 설정(순서 보장)
         axios({
             url: `${process.env.REACT_APP_REST_API_URL}/book/page/${page}/size/${size}`,
             method: "get"
@@ -16,6 +24,8 @@ const BookInfinite = () => {
 
                 setBookList([...bookList, ...response.data]);//spread 연산자
                 // setBookList(bookList.concat(...response.data));//concat 함수
+
+                loading.current = false;//로딩완료로 설정(순서 보장)
             })
             .catch(error => {
 
@@ -26,16 +36,74 @@ const BookInfinite = () => {
         loadBook();
     }, [page])
 
+    //다음페이지
     const nextPage = () => {
         setPage(page + 1);//페이지 1증가
         loadBook();
     }
+
     //개수가 변하면 페이지를 1로, 목록을 모두 지우고 다시 불러와야 한다.
-    useEffect(() => {      
-            setPage(1);
-            setBookList([]);
+    useEffect(() => {
+        setPage(1);
+        setBookList([]);
         //  loadBook();
     }, [size])
+    /////////////////////////////////////////////////////////////////////////
+    //스크롤 이벤트처리(scroll event handing)
+    // - 화면이 실행되면 window에 scroll이벤트 설정
+    // - 화면이 해제되기 직전에 이벤트 해제
+    /////////////////////////////////////////////////////////////////////////
+    useEffect(() => {
+        if (loading.current == true) {//로딩이 진행중인 상황
+            return;//그만해
+        }
+
+        //화면 생성 시 할 작업
+        const listner = throttle(() => {
+            // console.log("스크롤이 굴러갑니다!")
+            const percent = calculateScrollPercentage();
+            // console.log("퍼센트=", percent);
+            if (percent >= 75) { nextPage(); }
+        }, 750);
+
+        window.addEventListener("scroll", listner);
+        console.log("걸렸다");
+
+        //화면 해제 시 할 적업
+        return () => {
+            window.removeEventListener("scroll", listner);
+            console.log("풀렸다");
+        };
+    });
+
+    //스크롤 퍼센트 구하는 함수(Feat.GPT)
+    const calculateScrollPercentage = () => {
+        // 현재 스크롤 위치
+        var scrollPosition = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+
+        // 문서의 전체 높이
+        var documentHeight = Math.max(
+            document.body.scrollHeight,
+            document.body.offsetHeight,
+            document.documentElement.clientHeight,
+            document.documentElement.scrollHeight,
+            document.documentElement.offsetHeight
+        );
+
+        // 브라우저의 뷰포트 높이
+        var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+        // 스크롤 퍼센트 계산
+        var scrollPercentage = (scrollPosition / (documentHeight - windowHeight)) * 100;
+
+        return scrollPercentage;
+    }
+
+    // 스크롤 이벤트가 발생할 때 퍼센트를 출력하는 예제
+    window.addEventListener('scroll', function () {
+        var percentage = calculateScrollPercentage();
+        console.log('스크롤 퍼센트: ' + percentage.toFixed(2) + '%');
+    });
 
     return (
         <div className="container">
